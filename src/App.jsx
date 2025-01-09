95fe3fcce1faad1991a8c626abedc9c515640b0c
frndhoon <frndhoon@gmail.com>
2025-01-09 23:24:38 +0900

    feat: show Excalidraw

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Excalidraw } from '@excalidraw/excalidraw';
+
 function App() {
-  return <div className="text-3xl font-bold">그림톡</div>;
+  return (
+    <div className="h-screen w-screen bg-gray-100 custom-styles">
+      <div className="text-center p-4 bg-white shadow-md">
+        <p className="text-2xl font-bold">Excalidraw</p>
+        <p className="text-xl font-medium">grim-talk</p>
+      </div>
+      <div className="w-full h-full p-4">
+        <Excalidraw />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Excalidraw가 Node.js의 process.env를 필요로 하므로
+  // 브라우저 환경에서도 빈 객체라도 제공해주어야 함
+  // 이는 Node.js 환경의 process.env를 브라우저에서 흉내내는 역할을 함
+  define: {
+    'process.env': {},
+  },
+});
